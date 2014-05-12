c71ae6bc5fae8a8c5a6c5f2307d43e9a0268d882
bryanli <bryanlizhaolong@gmail.com>
2014-05-12 12:28:30 -0400

    Added test script for ActionController.cs

---
+++ CubeBehavior.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+// Test script for the ActionController
+
+public class CubeBehavior : ActionController {
+
+	// Use this for initialization
+	void Start () {
+		//ConstForward (rigidbody, 2F);
+		Jump (rigidbody, 500);
+		//transform.rotation = SpinX (transform, 40);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveForward(transform, 2F, Time.deltaTime);
+		transform.rotation = SpinY (transform, 2);
+		//rigidbody.velocity = new Vector3 (0, 0, 1);
+		//transform.position = MoveEast(transform, 2F, Time.deltaTime);
+		//transform.position = MoveRight (transform, 2F, Time.deltaTime);
+		//Vector3 vec = new Vector3 (0, 1, 0);
+		//transform.Rotate (vec, 2F);
+		//transform.Translate (Vector3.forward * 2F * Time.deltaTime);
+		//transform.position += Vector3.forward * 2F * Time.deltaTime;
+	}
+}
